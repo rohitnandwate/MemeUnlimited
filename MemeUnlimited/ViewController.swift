d80e521a65f960f64fb3f71fc051d65c89ba9c73
Rohit Nandwate <nandwate@amazon.com>
2017-02-23 22:28:28 -0500

    Initial Commit

---
+++ MemeUnlimited/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MemeUnlimited
+//
+//  Created by Nandwate, Rohit on 2/23/17.
+//  Copyright © 2017 Nandwate, Rohit. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
